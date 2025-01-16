80c2dd8b9b4d5853e711abf496c1d52b7f9ac595
faisal isfahani <workwfaisal@gmail.com>
2025-01-17 02:58:40 +0700

    feat: add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BLOB_READ_WRITE_TOKEN:"vercel_blob_rw_d7wSOoflR505PrRO_r0MwJ3y2F5axyN7sx4tPMGlwvJLm3t"
+
+  }
 }
 
 module.exports = nextConfig
